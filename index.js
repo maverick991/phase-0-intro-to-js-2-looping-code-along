399b444258f9678a3fdca568325edd0346f0a48e
MaverickEdwards <r.edwards0917@gmail.com>
2022-11-09 17:41:31 -0500

    commit tree f8d5b8c6bc7607fb0a2114548762a74523673699

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(cards){
+    let birthdayMessages = [];
+    for (let i = 0; i < cards.length; i++) {
+        birthdayMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return birthdayMessages;
+}
+function countDown(number){
+    let b = 11;
+    while (b > 0){
+        console.log(number);
+        b--;
+        number-=1;
+    }
+    return number;
+}
